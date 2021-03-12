d56cb868a78f7ba43513f705a088d908a9b3be80
Pin-Yu Wang <pywang@datalab.cs.nthu.edu.tw>
2021-03-12 13:46:14 +0800

    Add a field sugar in menu json

--- src/domain/entities/ice.go
+++ src/domain/entities/ice.go
@@ -5,3 +5,8 @@ type Ice struct {
 	ID  uint   `yaml:"ice_id" json:"ice_id" gorm:"primaryKey"`
 	Tag string `yaml:"ice_tag" json:"ice_tag" gorm:"not null"`
 }
+
+// IsHotID return true if this ID means hot
+func (ice *Ice) IsHotID() bool {
+	return ice.ID == 1
+}
